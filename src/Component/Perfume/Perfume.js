de764af9c76b4f34b3fe31fbb016a8e214535633
Hashinur Sohan <hashinur.sohan@outlook.com>
2023-03-19 21:03:11 +0600

    display product done

--- src/Component/Perfume/Perfume.js
+++ src/Component/Perfume/Perfume.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import './perfume.css';
 
 const Perfume = (props) => {
-     console.log(props.perfume)
+     //console.log(perfume)
      console.log(typeof props.perfume.pic)
-    const {name, pic, price} =props.perfume;
+    const {name, pic, price, company} =props.perfume;
+  //  console.log(typeof perfume.pic)
     
     return (
-        <div>
-            { <h2>Name: {name}</h2> }
+        <div className='showData'>
             {<img src={pic} alt=''></img>}
+            { <h2>Name: {name}</h2> }
+            
             {<h2> Price: {price}</h2>}
+            {<h2> Brand: {company}</h2>}
         </div>
+        
     );
 };
 
